d26bbf659d8037573cfdb31ca7c42920b92a61e2
dfeverx <dfeverx@gmail.com>
2021-03-07 18:23:28 +0530

    fix error in ci/cd

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,7 +16,6 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
